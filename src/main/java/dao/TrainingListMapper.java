c7843bfc46243c09c6564badcc81bffa7d149802
SonicBagBang <pgcdfff@gmail.com>
2021-05-21 01:09:51 +0800

    version1.0

--- src/main/java/dao/TrainingListMapper.java
+++ src/main/java/dao/TrainingListMapper.java
@@ -6,5 +6,7 @@ public interface TrainingListMapper {
 
     void uoloadTrainingList(TrainingList trainingList);
 
-    TrainingList downloadTrainingList();
+    TrainingList downloadTrainingList(String GUID);
+    void downloadAllTraining();
+
 }
--- src/main/java/dao/impl/TrainingListMapperImpl.java
+++ src/main/java/dao/impl/TrainingListMapperImpl.java
@@ -2,16 +2,31 @@ package dao.impl;
 
 import dao.TrainingListMapper;
 import entity.TrainingList;
+import utils.FileLoader;
+import utils.JsonData;
 
 public class TrainingListMapperImpl implements TrainingListMapper {
 
-    @Override
-    public void uoloadTrainingList(TrainingList trainingList) {
 
+    public void uoloadTrainingList(TrainingList trainingList) {
+        JsonData.GetCandidateTeacher().add(trainingList);
     }
 
-    @Override
-    public TrainingList downloadTrainingList() {
-        return null;
+    public TrainingList downloadTrainingList(String GUID)
+    {
+        TrainingList result=null;
+        for(TrainingList list:JsonData.GetCandidateTeacher())
+        {
+            if(list.getGUID().equals(GUID))
+            {
+                result=list;
+            }
+        }
+        return result;
+    }
+    /**下载全部注册*/
+    public void downloadAllTraining()
+    {
+        JsonData.commit();
     }
 }
