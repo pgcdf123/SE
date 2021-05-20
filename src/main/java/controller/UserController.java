b86eb001cceee81b1aba8c694860a7bd8a61fabe
FlyingDutchmanLQY <46863356+FlyingDutchmanLQY@users.noreply.github.com>
2021-05-21 02:29:03 +0800

    Login returns a User

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,8 +1,10 @@
 package controller;
 
+import entity.User;
+
 public interface UserController {
 
     void register(int id, String GUID, String password);
 
-    void login(String GUID, String password);
+    User login(String GUID, String password);
 }
